bacbe0da607a71c6c7eebb61ef1c94c8c1fbe916
Shailja <shailjashaktawat04@gmail.com>
2025-02-12 13:16:47 +0530

    Adding semaphore code updates

--- unit2/code1.c
+++ unit2/code1.c
@@ -4,67 +4,55 @@
 #include <unistd.h>
 #include <string.h>
 
-sem_t mutex
+sem_t mutex;
 
 void *my_thread_1(void *arg) {
+    char s[] = "abcdefgh";
+    int l = strlen(s);
 
-    char s[]= "abcdefgh",
+    for (int i = 0; i < l; i++) {
+        // wait
+        sem_wait(&mutex);
 
-    int l strlen(s);
+        // critical section
+        putchar(s[i]);
+        fflush(stdout);
+        sleep(2);
 
-    for(int 10, 11, 1)
-    {
-    //walt
-
-    sem_wait(&mutex);
-
-    //critical section
-    putchar(s[i]);
-    fflush(stdout);
-    sleep(2);
-
-    //signal
-    sem_post(&mutex);
-
+        // signal
+        sem_post(&mutex);
     }
 
+    return NULL;
 }
 
 void *my_thread_2(void *arg) {
+    char s[] = "ABCDEFGH";
+    int l = strlen(s);
 
-    char s[]= "ABCDEFGH";
+    for (int i = 0; i < l; i++) {
+        // wait
+        sem_wait(&mutex);
 
-    int l strlen(s);
+        // critical section
+        putchar(s[i]);
+        fflush(stdout);
+        sleep(2);
 
-    for(int 10, 11, 1)
-    {
-    //walt
-
-    sem_wait(&mutex);
-
-    //critical section
-    putchar(s[i]);
-    fflush(stdout);
-    sleep(2);
-
-    //signal
-    sem_post(&mutex);
-
+        // signal
+        sem_post(&mutex);
     }
 
+    return NULL;
 }
 
-
-int main(){
-    
+int main() {
     pthread_t t1, t2;
 
     sem_init(&mutex, 0, 1);
 
     pthread_create(&t1, NULL, my_thread_1, NULL);
-
-
-
+    sleep(2);
     pthread_create(&t2, NULL, my_thread_2, NULL);
 
     pthread_join(t1, NULL);
